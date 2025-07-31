5f9c5fd7629e931eed1540f1b3371229a6a51be2
k3nder <k3nder@outlook.es>
2025-07-31 17:58:31 +0200

    feat(compression): Add compressions - Easy use trait Decoder and Factory

--- src/decompress/mod.rs
+++ src/decompress/mod.rs
@@ -1,15 +1,45 @@
-use std::path::{Path, PathBuf};
+use std::{
+    fs::{self, File},
+    io::Read,
+    path::{Path, PathBuf},
+};
 
-pub enum ArchiveEntry {
-    File(String),
-    Directory(Box<Vec<ArchiveEntry>>),
-    Symlink(String),
-}
+use tracing::warn;
+
+#[cfg(feature = "zip")]
+pub mod zip;
+
+#[cfg(feature = "gz")]
+pub mod gz;
+
+#[cfg(feature = "tar")]
+pub mod tar;
+
+#[cfg(feature = "xz")]
+pub mod xz;
+
+#[cfg(feature = "targz")]
+pub mod targz;
+
+#[cfg(feature = "tarxz")]
+pub mod tarxz;
+
 pub trait DecoderFactory {
-    fn from_file(file: PathBuf) -> std::io::Result<Box<dyn Decoder>>;
-    fn supports(file: &Path) -> bool;
+    fn from_bytes(bytes: Vec<u8>) -> std::io::Result<Box<dyn Decoder>>;
+    fn from_file<P: AsRef<Path>>(file: P) -> std::io::Result<Box<dyn Decoder>> {
+        let file = fs::read(file)?;
+        Self::from_bytes(file)
+    }
+    fn supports_file<P: AsRef<Path>>(file: P) -> bool {
+        let bytes = fs::read(file);
+        if let Err(err) = bytes {
+            warn!("Error reading file in type checking: \n{}", err.to_string());
+            return false;
+        }
+        Self::supports(&bytes.unwrap())
+    }
+    fn supports(file: &[u8]) -> bool;
 }
 pub trait Decoder {
-    fn entries(&self) -> Vec<ArchiveEntry>;
-    fn extract(&self, inner: String, destination: PathBuf);
+    fn extract(&mut self, destination: PathBuf) -> Result<(), String>;
 }
