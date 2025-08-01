b3df109c6e02f0fe33368be3af79fce778f49ab9
k3nder <k3nder@outlook.es>
2025-08-01 22:15:07 +0200

    add common error handles

--- src/cas/mod.rs
+++ src/cas/mod.rs
@@ -1,41 +1,26 @@
-use std::path::PathBuf;
+use std::{fmt::Debug, path::PathBuf};
 
 use tracing::debug;
 
-use crate::hash::Hasher;
-use std::{io, path::Path};
-
 pub mod default;
 
-pub trait Store {
-    type Hash: Hasher;
-    type Err: std::fmt::Display + From<std::io::Error>;
-    fn write(&mut self, file: Vec<u8>) -> Result<PathBuf, Self::Err>;
-    fn create<P: AsRef<Path>>(&mut self, file: Vec<u8>, dst: P) -> Result<(), Self::Err> {
+/// Define a new store that manage a cas system
+/// Implements:
+///  - [Send]
+///  - [Sync]
+///  - [Debug]
+pub trait Store: Send + Sync + Debug {
+    /// Write a new file in hash filesystem and return the path
+    fn write(&self, file: Vec<u8>) -> Result<PathBuf, String>;
+    /// Write a new file and create a symlink to the original path
+    fn create(&self, file: Vec<u8>, dst: PathBuf) -> Result<(), String> {
         let src = self.write(file)?;
         debug!(
             "Creating symlink {} -> {}",
-            dst.as_ref().to_string_lossy(),
+            dst.to_string_lossy(),
             src.to_string_lossy()
         );
-        create_symlink(src, dst)?;
+        crate::utils::create_symlink(src, dst).map_err(|e| e.to_string())?;
         Ok(())
-    }
-}
-
-#[cfg(unix)]
-fn create_symlink<P: AsRef<Path>, Q: AsRef<Path>>(src: P, dst: Q) -> io::Result<()> {
-    std::os::unix::fs::symlink(src, dst)
-}
-
-#[cfg(windows)]
-fn create_symlink<P: AsRef<Path>, Q: AsRef<Path>>(src: P, dst: Q) -> io::Result<()> {
-    use std::fs;
-    let src = src.as_ref();
-
-    if src.is_dir() {
-        std::os::windows::fs::symlink_dir(src, dst)
-    } else {
-        std::os::windows::fs::symlink_file(src, dst)
     }
 }
